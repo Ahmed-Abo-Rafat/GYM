98cc2050a21959431d834f37722912ed6948852d
Ahmed Rafat <71386076+Ahmed-Abo-Rafat@users.noreply.github.com>
2024-07-31 21:16:19 +0300

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions = {
     
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '8ade854063msh3b8e2b7c6bbb227p1687aajsn3fbf3cc5f5be',
+    'X-RapidAPI-Key': '8b1b55e58amshfa718687a7d994bp1be775jsn637acc44c42a',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 }
